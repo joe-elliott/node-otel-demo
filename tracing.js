625dbebb97835855834706796a772f7a46a7fef0
Joe Elliott <number101010@gmail.com>
2022-08-04 17:08:17 -0400

    this technically works
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- tracing.js
+++ tracing.js
@@ -4,12 +4,15 @@
 const opentelemetry = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://agent:4318/v1/traces',
+  }),
   instrumentations: [getNodeAutoInstrumentations()]
 });
 
